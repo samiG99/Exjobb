090c25c2eba6118194559d18ed0e1ce027943684
gormoh201 <gormoh201@ubuntu.myguest.virtualbox.org>
2023-02-13 20:41:49 +0100

    redigerad_code

---
+++ read_bag.py
@@ -0,0 +1,37 @@
+import rosbag 
+import cv2
+import numpy as np 
+
+arr = []
+def readBagFile():
+    readingBagFile = rosbag.Bag("2022-08-26-15-18-39.bag")
+    for topic, msg, t in readingBagFile.read_messages(topics=['/robot/k4a_top/depth/image_rect']):
+        arr.append(msg)
+        #print("Message",msg)
+    
+    if len(arr)<10:
+        raise ValueError ("teh size of array is not enought")
+
+    readingBagFile.close()
+
+def ConvertTOFrame():
+    for msg in arr:
+        #convert the msg data to binary data, the string to binary, binary to bytearray  
+        BIN_DATA = bytearray(msg.data)
+        
+        #converting the msg to frame 
+        frame = np.array(BIN_DATA, dtype= np.uint16)
+        frame = frame.reshape(msg.height, msg.width,-1) #we tell the numpy to compute the size of thrid dimension based of the size of the array
+        print(frame)
+        #use opencv to process the frame
+        processTheFrame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY )
+        cv2.imshow("processTheFrame" ,processTheFrame)
+        key = cv2.waitKey(80)
+        if key ==27:
+            break
+    cv2.destroyAllWindows()
+
+
+
+readBagFile()
+ConvertTOFrame()
