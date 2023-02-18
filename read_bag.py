7644a73904cee8debe9f27bdec3a02f5f66dbb67
gormoh201 <gormoh201@ubuntu.myguest.virtualbox.org>
2023-02-18 11:37:46 +0100

    Read and convert bagfile to video

--- read_bag.py
+++ read_bag.py
@@ -4,8 +4,8 @@ import numpy as np
 
 arr = []
 def readBagFile():
-    readingBagFile = rosbag.Bag("2022-08-26-15-18-39.bag")
-    for topic, msg, t in readingBagFile.read_messages(topics=['/robot/k4a_top/depth/image_rect']):
+    readingBagFile = rosbag.Bag("2022-08-26-15-20-17.bag")
+    for topic, msg, t in readingBagFile.read_messages(topics=['/robot/fisheye_front/image_color']):
         arr.append(msg)
         #print("Message",msg)
     
@@ -15,17 +15,22 @@ def readBagFile():
     readingBagFile.close()
 
 def ConvertTOFrame():
-    for msg in arr:
+    for msg in arr[20:]:
         #convert the msg data to binary data, the string to binary, binary to bytearray  
         BIN_DATA = bytearray(msg.data)
         
         #converting the msg to frame 
-        frame = np.array(BIN_DATA, dtype= np.uint16)
+        frame = np.array(BIN_DATA, dtype= np.uint8)
         frame = frame.reshape(msg.height, msg.width,-1) #we tell the numpy to compute the size of thrid dimension based of the size of the array
-        print(frame)
+        #print(frame)
+        #print(np.max(frame))
         #use opencv to process the frame
-        processTheFrame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY )
+        processTheFrame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+        #print(processTheFrame)
+        #print(processTheFrame.shape)
+        #print(np.max(processTheFrame))
         cv2.imshow("processTheFrame" ,processTheFrame)
+        #cv2.imshow("Frame" ,frame[:, :, :3])
         key = cv2.waitKey(80)
         if key ==27:
             break
